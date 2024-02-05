9b8e215908411d2e343cb5648132781a63e75dbc
Yosef Yan <yosefully@gmail.com>
2024-02-05 23:19:57 +0200

    fix

--- src/main.tsx
+++ src/main.tsx
@@ -5,8 +5,6 @@ import "./css/index.css";
 import { HashRouter } from "react-router-dom";
 import "react-toastify/dist/ReactToastify.css";
 
-// לעשות ש.ב
-
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
     <HashRouter>
