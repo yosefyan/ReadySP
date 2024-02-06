9d98fb53358e9f05abceea0cfea6352739c7ce32
Yosef Yan <yosefully@gmail.com>
2024-02-06 16:52:45 +0200

    Fix

---
+++ src/layout/linksLayout.ts
@@ -0,0 +1,73 @@
+import ROUTES from "../routes/ROUTES";
+import {
+  FaHome,
+  FaPencilAlt,
+  IoMdLogIn,
+  GiFallingStar,
+  MdDashboard,
+  MdWavingHand,
+  BsCardHeading,
+  BsPerson,
+  IoAddCircle,
+  FaHeart,
+  BiSolidCollection,
+} from "../constants/iconsData";
+
+const generateLinks = (
+  times: number,
+  route: string[],
+  comp: string[],
+  icon: React.ReactElement[]
+) => {
+  return Array.from({ length: times }, (_, i) => ({
+    to: route[i],
+    children: route[i].replace("/", ""),
+    comp,
+    icon: icon[i],
+  }));
+};
+
+const defaultData = {
+  defaultLinks: generateLinks(
+    2,
+    [ROUTES.HOME, ROUTES.ABOUT],
+    ["Home", "About"],
+    [FaHome, GiFallingStar]
+  ),
+
+  defaultLoggediInLinks: generateLinks(
+    1,
+    [ROUTES.PROFILE],
+    ["Profile"],
+    [BsPerson]
+  ),
+};
+
+const welcomeLinks = generateLinks(
+  3,
+  [ROUTES.WELCOME, ROUTES.LOGIN, ROUTES.REGISTER],
+  ["Welcome", "Login", "Register"],
+  [MdWavingHand, IoMdLogIn, FaPencilAlt]
+);
+
+const loggedInLinks = generateLinks(
+  1,
+  [ROUTES.FAVCARDS],
+  ["FavCards"],
+  [BsCardHeading]
+);
+
+const businessLinks = generateLinks(
+  3,
+  [ROUTES.FAVCARDS, ROUTES.MYCARDS, ROUTES.CREATECARD],
+  ["FavCards", "MyCards", "CreateCard", "EditCard"],
+  [FaHeart, BiSolidCollection, IoAddCircle]
+);
+const adminLinks = generateLinks(
+  4,
+  [ROUTES.FAVCARDS, ROUTES.MYCARDS, ROUTES.CREATECARD, ROUTES.CRM],
+  ["FavCards", "MyCards", "CreateCard", "CRM", "EditCard"],
+  [FaHeart, BiSolidCollection, IoAddCircle, MdDashboard]
+);
+
+export { defaultData, welcomeLinks, loggedInLinks, businessLinks, adminLinks };
