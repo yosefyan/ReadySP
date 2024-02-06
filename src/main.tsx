86c37331ca47f8291348f30a801b9216ab5a91a0
Yosef Yan <yosefully@gmail.com>
2024-02-06 15:56:53 +0200

    f

--- src/main.tsx
+++ src/main.tsx
@@ -7,7 +7,7 @@ import "react-toastify/dist/ReactToastify.css";
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
-    <HashRouter basename="/ReadySP">
+    <HashRouter >
       <App />
     </HashRouter>
   </React.StrictMode>
